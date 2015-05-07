7c5ab4245484a37c6318315b3fd42fcc87b012b3
Stefano Mondino <stefano.mondino.dev@gmail.com>
2015-05-07 20:01:27 +0200

    ifx

--- mail.php
+++ mail.php
@@ -5,13 +5,20 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
-$from = $name .' <'.$email.'>';
+$from = 'From: '$name .' <'.$email.'>';
 
 
 if ($name == NULL || $email == NULL || $message == NULL) {
   echo "All field are mandatory. Please fill them all";
 }
 else {
+
+  $headers = $from;
+  $to = 'stefano.mondino.dev@gmail.com';
+  $subject = 'Francomondino.it - '.$name.' wants to ask you something.'
+
+  mail($to, $subject, $message, $headers);
+
   echo "Thank you! Your message was sent. I'll get in touch as soon as possible.";
 }
 ?>
